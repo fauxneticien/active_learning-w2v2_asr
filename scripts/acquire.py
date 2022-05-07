992a957b71c61a52970c6f75b2e70acaeddfd676
meredithxu <yiyangxu@umail.ucsb.edu>
2022-05-07 16:28:18 -0700

    tested shell script

--- scripts/acquire.py
+++ scripts/acquire.py
@@ -26,17 +26,19 @@ pool_df = pd.read_csv(args.pool_tsv, sep='\t')
 on = ['path', 'text']
 
 if args.lpool_tsv:
+	# paths = args.lpool_tsv.split('/')
+	# paths[-2] = str(int(paths[-2])+1)
+	# itr_num = int(paths[-1].split('-')[-1][0])
+	# paths[-1] = f'train-{itr_num+1}.tsv'
+	# lpool_path =  '/'.join(paths)
 	lpool_df = pd.read_csv(args.lpool_tsv, sep='\t')
-    # upool_df = pool_df - lpool_df
 	upool_df = (pool_df.merge(lpool_df[on], on=on, how='left', indicator=True).query('_merge == "left_only"').drop('_merge', 1))
-	lpool_path = args.lpool_tsv
 
 else:
 	new_lpool_df = pd.DataFrame([], columns=on)
-	paths = args.newdata_tsv.split('/')
-	paths[-1] = 'lpool.tsv'
-	lpool_path = '/'.join(paths)
-	new_lpool_df.to_csv(lpool_path, sep='\t', index=False)
+	# paths = args.newdata_tsv.split('/')
+	# paths[-1] = 'lpool.tsv'
+	# lpool_path = '/'.join(paths)
 	upool_df = pool_df
 	lpool_df = new_lpool_df
 
@@ -61,21 +63,3 @@ assert len(lpool_df[lpool_df[on[0]].isin(new_data_df[on[0]])]) == 0, "Newly samp
 lpool_combined_df = pd.concat([lpool_df, new_data_df], sort=False)
 
 lpool_combined_df.to_csv(args.newdata_tsv, sep='\t', index=False)
-lpool_combined_df.to_csv(lpool_path, sep='\t', index=False)
-
-	# candidate = all unpool - acquired so far
-# on = ['path', 'text']
-# new_data_candidate_df = (pool_df.merge(lpool_df[on], on=on, how='left', indicator=True).query('_merge == "left_only"').drop('_merge', 1))
-# new_data_df = new_data_candidate_df.sample(n=int(args.frac*len(pool_df)), replace=False).reset_index(drop=True)
-
-# # run check to make 100% sure new_data and lpool are not overlapping. if so raise an error
-# assert len(lpool_df[lpool_df[on[0]].isin(new_data_df[on[0]])]) == 0, "Newly sampled data cannot overlap with acquired data so far from past."
-
-# # if check passed, combine lpool and new_data
-# lpool_combined_df = pd.concat([lpool_df, new_data_df])
-
-# # write to files 
-# lpool_combined_df.to_csv(args.newdata_tsv, sep='\t', index=False)
-# lpool_combined_df.to_csv(args.lpool_tsv, sep='\t', index=False)
-
-# acquire.py random all_data.tsv 
